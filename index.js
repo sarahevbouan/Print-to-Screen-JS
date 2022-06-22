fc9ce3a1b4a66213afba6a703bbaeb032c00ebae
Sarah Evbouan <sarahevbouan@yahoo.com>
2022-06-22 20:39:16 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let myName = "Sarah Evbouan";
+let height = "5.7 ft";
+let country = "Nigeria";
+
+//Print to screen
+alert("Name: " + myName + "\n" + "Height: " + height + "\n" + "Country: " + country);
